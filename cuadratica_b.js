1a5542780e9005de42c182f6ac6c549dff21927d
Funnym0th <52089905+funnym0th@users.noreply.github.com>
2022-07-31 22:54:06 -0400

    a

--- cuadratica_b.js
+++ cuadratica_b.js
@@ -1,4 +1,7 @@
-let ecuacion = {A:Number(prompt("Numero A:")), C:Number(prompt("Numero C:"))};
-let raiz = Math.sqrt(- 4 * ecuacion.A * ecuacion.C);
-let divisor = 2 * ecuacion.A
-alert("El valor de X1 es: " + Number((0 + raiz) / divisor) + "\nEl valor de X2 es: " + Number((0 - raiz) / divisor))
+var button = document.querySelector('button');
+button.onclick = function() {
+    let ecuacion = {A:Number(prompt("Numero A:")), C:Number(prompt("Numero C:"))};
+    let raiz = Math.sqrt(- 4 * ecuacion.A * ecuacion.C);
+    let divisor = 2 * ecuacion.A
+    alert("El valor de X1 es: " + Number((0 + raiz) / divisor) + "\nEl valor de X2 es: " + Number((0 - raiz) / divisor))
+}
--- cuadratica_completa.js
+++ cuadratica_completa.js
@@ -1,4 +1,7 @@
-let ecuacion = {A:Number(prompt("Numero A:")), B:Number(prompt("Numero B:")), C:Number(prompt("Numero C:"))};
-let raiz = Math.sqrt(Math.pow(ecuacion.B,2) - 4 * ecuacion.A * ecuacion.C);
-let divisor = 2 * ecuacion.A
-alert("El valor de X1 es: " + Number((-ecuacion.B + raiz) / divisor) + "\nEl valor de X2 es: " + Number((-ecuacion.B - raiz) / divisor))
+var button = document.querySelector('button');
+button.onclick = function() {
+    let ecuacion = {A:Number(prompt("Numero A:")), B:Number(prompt("Numero B:")), C:Number(prompt("Numero C:"))};
+    let raiz = Math.sqrt(Math.pow(ecuacion.B,2) - 4 * ecuacion.A * ecuacion.C);
+    let divisor = 2 * ecuacion.A
+    alert("El valor de X1 es: " + Number((-ecuacion.B + raiz) / divisor) + "\nEl valor de X2 es: " + Number((-ecuacion.B - raiz) / divisor))
+}
