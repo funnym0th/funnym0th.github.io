679d9b9d9cf0bc80a5179362000421033fe62d67
Funnym0th <52089905+funnym0th@users.noreply.github.com>
2022-08-17 15:00:34 -0400

    Add files via upload

---
+++ js/angulos.js
@@ -0,0 +1,13 @@
+let Cateto = { 
+    Alpha:prompt("Cateto A: "), 
+    Beta:prompt("Cateto B: ") 
+};
+
+let Hipotenusa = Math.sqrt(Cateto.Alpha ** 2 + Cateto.Beta ** 2);
+alert("Cateto A = " + Cateto.Alpha + ", Cateto B = " + Cateto.Beta + ", Hipotenusa = " + Hipotenusa);
+let Angulo = {
+    Alpha: Math.asin(Cateto.Beta / Hipotenusa) * 180 / Math.PI,
+}
+
+Angulo.Beta = 90 - Angulo.Alpha
+alert("El angulo alpha es: " + Angulo.Alpha.toFixed(1) + ", y el angulo beta es: " + Angulo.Beta.toFixed(1) + ".");
